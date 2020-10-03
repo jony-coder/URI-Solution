e00a436eeabf3a484d0c02f523ab7ae863fd9edb
Jahid Hasan Jony <66401188+jony-coder@users.noreply.github.com>
2020-10-03 21:54:32 +0600

    Add files via upload

---
+++ uri1041.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+
+    float X,Y;
+    scanf("%f", &X);
+    scanf("%f", &Y);
+
+    if(X==0.0&&Y==0.0){
+        printf("Origem\n");
+    }
+    else if(Y==0.0&&(X>0.0||X<0.0)){
+        printf("Eixo X\n");
+    }
+    else if(X==0.0&&(Y>0.0||Y<0.0)){
+        printf("Eixo Y\n");
+    }
+    else if(X>0.0&&Y>0.0){
+        printf("Q1\n");
+    }
+    else if(X<0.0&&Y>0.0){
+        printf("Q2\n");
+    }
+    else if(X<0.0&&Y<0.0){
+        printf("Q3\n");
+    }
+    else if(X>0.01&&Y<0.01){
+        printf("Q4\n");
+    }
+
+return 0;
+}
