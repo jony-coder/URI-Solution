87750fe044260f2e481b0d43219546b54a8f6c7f
Jahid Hasan Jony <66401188+jony-coder@users.noreply.github.com>
2021-05-20 16:43:21 +0600

    Add files via upload

---
+++ uri1114.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string n;
+    while(1){
+
+        cin>>n;
+        if(n=="2002"){
+            cout<<"Acesso Permitido"<<endl;
+            break;
+        }
+
+        else{
+
+            cout<<"Senha Invalida"<<endl;
+
+        }
+    }
+
+return 0;
+}
