8e023e054e6d2f485799945e1b816390643aa43c
Jahid Hasan Jony <66401188+jony-coder@users.noreply.github.com>
2021-05-31 21:34:12 +0600

    Add files via upload

---
+++ uri1174.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    float arr[100];
+    for(int i=0; i<100; i++){
+        cin>>arr[i];
+
+    }
+
+    for(int i =0; i<100; i++){
+    if(arr[i]<=10){
+            printf("A[%d] = %.1f\n", i, arr[i]);
+    }
+
+    }
+
+return 0;
+}
