dafa1b68989f767f94f7d0dbad89627ff3877960
Jahid Hasan Jony <66401188+jony-coder@users.noreply.github.com>
2021-06-15 10:58:17 +0600

    Add files via upload

---
+++ uri1095.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+        int i,j;
+    for(i=1,j=60; j>=0; j= j-5, i= i+3){
+        printf("I=%d J=%d\n", i,j);
+    }
+
+return 0;
+}
---
+++ uri1097.cpp
@@ -0,0 +1,18 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+        int i,j, k=7;
+    for(i=1; i<10; i=i+2){
+           for(j=0; j<3; j++){
+            printf("I=%d J=%d\n", i, k);
+            k--;
+           }
+           k = k + 5;
+
+    }
+
+return 0;
+}
+
