eaa2fc477267bc8db9965f5c3ad7ed5bb890fcb6
Jahid Hasan Jony <66401188+jony-coder@users.noreply.github.com>
2021-06-30 12:23:31 +0600

    Add files via upload

---
+++ uri1155.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    float sum=0.0;
+    for(int i=1; i<=100; i++){
+
+        sum = sum +((float)1/(float)i);
+
+
+    }
+
+    printf("%.2f\n", sum);
+
+return 0;
+}
