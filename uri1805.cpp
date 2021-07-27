1c1c28278ba53f3c904b07bcae781bb5e7f24d69
Jahid Hasan Jony <66401188+jony-coder@users.noreply.github.com>
2021-07-27 13:08:05 +0600

    Add files via upload

---
+++ uri1805.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    long long int x,y;
+    long long int sum=0;
+    cin>>x>>y;
+
+    sum = (y-x+1)*(x+y)/2;
+    cout<<sum<<endl;
+
+return 0;
+}
