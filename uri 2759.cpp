3b3e45e14995f95528f6a2019a3d5fed80d790c5
Jahid Hasan Jony <66401188+jony-coder@users.noreply.github.com>
2021-07-30 23:31:06 +0600

    Add files via upload

---
+++ uri 2759.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    char a,b,c;
+    cin>>a; cin>>b; cin>>c;
+
+    printf("A = %c, B = %c, C = %c\n", a, b, c);
+    printf("A = %c, B = %c, C = %c\n", b, c, a);
+    printf("A = %c, B = %c, C = %c\n", c, a, b);
+
+
+
+return 0;
+}
