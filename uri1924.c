5d4ee749532f294bfaa3cb957731824dc07b5aa2
Jahid Hasan Jony <66401188+jony-coder@users.noreply.github.com>
2021-07-31 11:52:13 +0600

    Add files via upload

---
+++ uri1924.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+    int T;
+    scanf("%d", &T);
+    char s[100];
+    int i;
+    for(i=0; i<T+1; i++){
+    gets(s);
+}
+
+    printf("Ciencia da Computacao\n");
+
+
+return 0;
+}
